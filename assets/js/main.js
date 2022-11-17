3e35230a8be379f3c6b474eca8a959fc112b5513
AlaminUI <alaminislam907@gmail.com>
2022-11-17 18:50:26 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,46 @@ function removeactive(){
 }
 
  
+$(function(){
+    // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('header').outerHeight();
+
+  $(window).scroll(function(event){
+    didScroll = true;
+  });
+
+  setInterval(function() { // wait for it... wait for it... wait for it
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 200);
+
+  function hasScrolled() {
+        var scrolltop = $(this).scrollTop();
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - scrolltop) <= delta)	return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (scrolltop > lastScrollTop && scrolltop > navbarHeight){
+          // On scroll down we add nav-up
+         
+          $('header').removeClass('header-sticky').addClass('nav-up');
+        
+        } else {
+          // On scroll up we add header-sticky
+          if(scrolltop + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('header-sticky');
+          }
+          if(scrolltop <200){
+            $('header').removeClass('header-sticky')
+          }
+        }
+
+        lastScrollTop = scrolltop;
+  }
+});
