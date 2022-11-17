9fb9b96b2ebb3ea376da5c68005270f131092647
AlaminUI <alaminislam907@gmail.com>
2022-11-17 12:08:16 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,20 @@ if (screen.width > 991){
 document.querySelector('.dropdown_toggle').addEventListener('click', function(){
     document.querySelector('.sub-menu').classList.toggle('open-dropdown');
 })
+
+
+for(i=0; i<document.querySelectorAll('.tab-item').length;i++){
+document.querySelectorAll('.tab-item')[i].addEventListener('click', function(){
+    removeactive();
+    this.classList.add('active');
+})
+}
+
+
+function removeactive(){
+    for(i=0; i<document.querySelectorAll('.tab-item').length;i++){
+        document.querySelectorAll('.tab-item')[i].classList.remove('active');
+    }
+}
+
+ 
